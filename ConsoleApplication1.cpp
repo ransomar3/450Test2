c8099d309429c694301e064ca3c5b5e031672dd4
ransomar3 <31740767+ransomar3@users.noreply.github.com>
2017-09-07 18:25:51 -0400

    Create ConsoleApplication1.cpp

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	char TDArray[5][7] = {
+		{ ' ','/','\\','_','/','\\',' ' },{ '/',' ','D',' ', 'D',' ','\\' },
+		{ '(','=','=','O','=','=',')' },{ '\\',' ',' ','~', ' ',' ','/' },
+		{ ' ','-','-','-', '-','-',' ' } };
+	int row, col;
+	for (row = 0; row < 5; row++)
+	{
+		for (col = 0; col < 7; col++)
+		{
+			cout << TDArray[row][col];
+		cout << endl;
+	}
+
+	// finish – display TD array to console
+	return 0;
+}
